0b72d5d757d06692b52fb404515a81c189caa11f
Kevin Jones <kevin@rocksolidknowledge.com>
2018-12-15 14:14:10 +0000

    Updated to use modules

--- todolistservice/src/main/kotlin/Application.kt
+++ todolistservice/src/main/kotlin/Application.kt
@@ -1,19 +1,20 @@
 package com.knowledgespike
 
 import io.ktor.application.*
-import io.ktor.http.*
 import io.ktor.response.*
 import io.ktor.routing.*
-import io.ktor.server.engine.*
-import io.ktor.server.netty.*
+import io.ktor.http.*
 
-fun main(args: Array<String>) {
-    val server = embeddedServer(Netty, 9080) {
-        routing {
-            get("/") {
-                call.respondText("Hello, world!", ContentType.Text.Html)
-            }
+fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
+
+@Suppress("unused") // Referenced in application.conf
+fun Application.module(testing: Boolean = false) {
+    routing {
+        get("/") {
+            call.respondText("Hello, world!", contentType = ContentType.Text.Plain)
         }
+
     }
-    server.start(wait = true)
 }
+
+
